139bd3d460028dbeb1f72c5c69ec6e67d90f943a
Chris <cbzoozoom7@gmail.com>
2023-05-09 13:18:55 -0500

    wrote breadthFirstTraversal() & fixed an issue where depthFirstTraversal() would consistantly crash when it was done & an issue depthFirstTraversal() would stop short. breadthFirstTraversal() works the same as depthFirstTraversal(), except it takes the oldest item from the queue instead of the newest.

--- graph.cpp
+++ graph.cpp
@@ -23,7 +23,7 @@ bool Graph::addEdge(int fromId, int toId) {
         Vertex *from = findVertex(fromId);
         Vertex *to = findVertex(toId);
         if (from && to) {
-            if (from->adjacent.insert(to).second && to->adjacent.insert(from).second) {
+            if (from->adjacent.insert(to).second && to->adjacent.insert(from).second) { //second is a bool incidating success.
                 added = true;
             }
         }
@@ -47,7 +47,9 @@ void Graph::depthFirstTraversal(int startId) {
     Vertex *current = findVertex(startId);
     vector<Vertex*> next;
     std::unordered_set<Vertex*> visited;
-    while (current) {
+    next.push_back(current);
+    while (current && !next.empty()) {
+        next.erase(next.end()-1);
         if (!visited.count(current)) {
             std::cout << current->data.id << ": \"" << current->data.str << "\"" << std::endl;
             for (unordered_set<Vertex*>::iterator i = current->adjacent.begin(); i != current->adjacent.end(); i++) {
@@ -58,14 +60,33 @@ void Graph::depthFirstTraversal(int startId) {
             visited.insert(current);
         }
         current = next.back();
-        next.erase(next.end()-1);
+    } 
+}
+void Graph::breadthFirstTraversal(int startId) {
+    std::cout << "\nBreadth First Traversal:" << std::endl;
+    Vertex *current = findVertex(startId);
+    vector<Vertex*> next;
+    std::unordered_set<Vertex*> visited;
+    next.push_back(current);
+    while (current && !next.empty()) {
+        next.erase(next.begin());
+        if (!visited.count(current)) {
+            std::cout << current->data.id << ": \"" << current->data.str << "\"" << std::endl;
+            for (unordered_set<Vertex*>::iterator i = current->adjacent.begin(); i != current->adjacent.end(); i++) {
+                if (!visited.count(*i)) 
+                    next.push_back(*i);
+                }
+            
+            visited.insert(current);
+        }
+        current = next.front();
     }
 }
 Graph::~Graph() {
     clear();
 }
 void Graph::clear() {
-
+    
 }
 int Graph::getNumVertices() {
     return count;
@@ -80,9 +101,6 @@ void Graph::clearDisconnected() {
 bool Graph::removeVertex(int id) {
     bool removed = false;
     return removed;
-}
-void Graph::breadthFirstTraversal(int startId) {
-
 }
 bool Graph::getVertex(int id, Data *d) {
     return false;
